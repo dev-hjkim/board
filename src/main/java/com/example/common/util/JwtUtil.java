a95de098fdad6adb07ca06ba283632842490e228
dev-hjkim <dev--test@naver.com>
2022-04-29 01:59:10 +0900

    jwt 토큰 생성, 유효성 검사 로직 추가

---
+++ src/main/java/com/example/common/util/JwtUtil.java
@@ -0,0 +1,75 @@
+package com.example.common.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generate(String userSeq, String type) {
+
+        // 1. token 내부에 저장할 정보
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("seq", userSeq);
+        
+        // 2. token 생성일
+        final Date createdDate = new Date();
+
+        // 3. token 만료일
+        long expirationTime;
+        if ("ACCESS".equals(type)) {
+            // 테스트용(30분)
+            expirationTime = Long.parseLong(expiration) * 500;
+        } else {
+            // 테스트용(1시간)
+            expirationTime = Long.parseLong(expiration) * 1000;
+        }
+        final Date expirationDate = new Date(createdDate.getTime() + expirationTime);
+
+
+        return Jwts.builder()
+                .setClaims(claims)      // 1
+                .setIssuedAt(createdDate)       // 2
+                .setExpiration(expirationDate)      // 3
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean isExpired(String token) {
+        final Date expiration = getClaimsFromToken(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    public String getUserSeqFromToken(String token) {
+        return getClaimsFromToken(token).get("seq", String.class);
+    }
+
+    private JwtUtil() { }
+}
--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,6 +1,11 @@
 package com.example.filter;
 
+import com.example.common.dto.Result;
+import com.example.common.dto.ResultType;
+import com.example.common.util.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -13,6 +18,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AuthFilter implements Filter {
 
+    private static JwtUtil jwtUtil;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -24,13 +31,35 @@ public class AuthFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         // Request Header 에 Authorization 이 존재하지 않을 때
         if (httpRequest.getHeader("Authorization") == null) {
-            httpResponse.setStatus(403);
+            try {
+                httpResponse = setHttpServletResponse(httpResponse, ResultType.ACCESS_TOKEN_REQUIRED);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
 
         // Request Header 에서 token 문자열 받아오기
         String token = httpRequest.getHeader("Authorization");
+        if (jwtUtil.isExpired(token)) {
+            try {
+                httpResponse = setHttpServletResponse(httpResponse, ResultType.EXPIRED_ACCESS_TOKEN);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        String userSeq = jwtUtil.getUserSeqFromToken(token);
 
         chain.doFilter(httpRequest, httpResponse);
+    }
+
+    private HttpServletResponse setHttpServletResponse(HttpServletResponse response, ResultType resultType) throws IOException {
+        Result result = new Result(resultType);
+        response.setStatus(result.getHttpCode().value());
+        response.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        response.getOutputStream()
+                .print(new ObjectMapper().writeValueAsString(result));
+        return response;
     }
 
     @Override
