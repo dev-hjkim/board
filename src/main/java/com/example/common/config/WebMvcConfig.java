df1d38c2b41f602905b97641121b8504be6e14a7
dev-hjkim <dev--test@naver.com>
2022-05-10 02:34:50 +0900

    add interceptor config

---
+++ src/main/java/com/example/common/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.common.config;
+
+import com.example.interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .excludePathPatterns("/v1/auth/**");
+    }
+}
