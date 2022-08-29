ec6e1bd8fe37b6f32da2cbbbc1b3fa8cede329b5
rey <rey@f-lab.kr>
2022-08-29 22:35:36 +0900

    JPA 처음 세팅

--- src/main/java/com/example/auth/model/Member.java
+++ src/main/java/com/example/auth/model/Member.java
@@ -4,21 +4,31 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "tb_member")
 @Getter
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Member {
+    @Id
+    @Column(name = "member_no")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonIgnore
     private long memberNo;
 
+    @Column(name = "user_id")
     private String userId;
 
     @JsonIgnore
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "reg_dt")
     private Date regDt;
+    @Column(name = "upd_dt")
     private Date updDt;
 
     @Builder
--- src/main/java/com/example/auth/repository/AuthRepository.java
+++ src/main/java/com/example/auth/repository/AuthRepository.java
@@ -2,11 +2,17 @@ package com.example.auth.repository;
 
 import com.example.auth.model.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Mapper
-public interface AuthRepository {
-    boolean isDuplicated(String userId);
-    void signin(Member member);
-    Member findUserById(String userId);
-    Member findUserByUserSeq(long memberNo);
+//@Mapper
+@Repository
+public interface AuthRepository extends JpaRepository<Member, Long> {
+    Member findByUseId(String userId);
+    Member findByMemberNo(long memberNo);
+    boolean existsByUserId(String userId);
+//    boolean isDuplicated(String userId);
+//    void signin(Member member);
+//    Member findUserById(String userId);
+//    Member findUserByUserSeq(long memberNo);
 }
