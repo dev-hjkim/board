6110aa83336a98dbbee1c53773673e02a2fb66eb
dev-hjkim <dev--test@naver.com>
2022-09-15 00:02:37 +0900

    @NoArgs 어노테이션 추가

--- src/main/java/com/example/comment/dto/PathVariableSequenceDto.java
+++ src/main/java/com/example/comment/dto/PathVariableSequenceDto.java
@@ -2,8 +2,10 @@ package com.example.comment.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PathVariableSequenceDto {
     private long userSeq;
     private long boardSeq;
